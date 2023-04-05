f6b46fbe04a2d61543bc022006b5db0f7e6bcac8
Adam Poper <adampoper@Adams-MacBook-Pro.local>
2023-04-04 22:49:07 -0400

    Added a bond yield curve

--- src/app/core/components/bonds-component/bonds.component.ts
+++ src/app/core/components/bonds-component/bonds.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BondsService } from '../../services/bonds.service';
 import { Bond } from '../../models/bond';
 import { AppUtil } from '../../util/app-util';
+import { ChartConfiguration } from 'chart.js';
 
 @Component({
   selector: 'app-bonds-component',
@@ -11,21 +12,46 @@ import { AppUtil } from '../../util/app-util';
 export class BondsComponent implements OnInit {
 
   public bonds = new Array<Bond>();
+  public yieldCurveConfigData: ChartConfiguration['data'] | undefined;
+  public yieldCurveConfigOptions: ChartConfiguration['options'] | undefined;
 
   constructor(private bondService: BondsService) {
 
   }
 
   ngOnInit(): void {
-    this.fetchBondValues();
-  }
-
-  public fetchBondValues() {
     this.bondService.fetchUSTreasuries()
-      .subscribe(bonds => this.bonds = bonds);
+      .subscribe((bonds: Bond[]) => {
+        this.bonds = bonds;
+        this.initYieldCurveConfig();
+      });
   }
 
   public roundYield(yieldChange: number): number {
     return AppUtil.round(yieldChange, 2);
+  }
+
+  private initYieldCurveConfig() {
+    this.yieldCurveConfigData = {
+      datasets: [{
+        data: this.bonds.map(bond => bond.couponRate),
+        fill: false,
+        backgroundColor: '#3485d1',
+        label: undefined
+      }],
+      labels: this.bonds.map(bond => bond.label)
+    };
+    this.yieldCurveConfigOptions = {
+      plugins: {
+        legend: {
+          display: false
+        }
+      },
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
   }
 }
