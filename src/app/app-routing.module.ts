7ee7ba07a0ab4948d9a4f267929448d8b4aaa9f3
Adam Poper <adampoper@Adams-MacBook-Pro.local>
2025-01-17 22:24:29 -0500

    adding logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,16 @@ import { BondsComponent } from './core/components/bonds-component/bonds.componen
 import { CommoditiesComponent } from './core/components/commodities-component/commodities.component';
 import { WatchListsComponent } from './core/components/watch-lists/watch-lists.component';
 import { LoginComponent } from './core/components/login-component/login.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'stocks', component: StocksComponent },
-  { path: 'stocks/:symbol', component: StocksComponent },
-  { path: 'bonds', component: BondsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'stocks', component: StocksComponent, canActivate: [AuthGuard] },
+  { path: 'stocks/:symbol', component: StocksComponent, canActivate: [AuthGuard] },
+  { path: 'bonds', component: BondsComponent, canActivate: [AuthGuard] },
   // { path: 'commodities', component: CommoditiesComponent },
-  { path: 'watchLists', component: WatchListsComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'watchLists', component: WatchListsComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
--- src/app/core/query/user.query.ts
+++ src/app/core/query/user.query.ts
@@ -3,6 +3,7 @@ import { User } from "../models/user";
 import { UserState, UserStore } from "../store/user.store";
 import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
+import { CURRENT_USER } from "../services/auth.service";
 
 @Injectable({providedIn: 'root'})
 export class UserQuery extends Query<UserState> {
@@ -13,7 +14,16 @@ export class UserQuery extends Query<UserState> {
         super(store);
     }
 
-    public selectCurrentUser() {
+    public selectCurrentUser(): Observable<User> {
         return this.select(state => state.currentUser);
+    }
+
+    public getUser(): User {
+        let user = this.getValue().currentUser;
+        if (!user)  {
+            user = JSON.parse(sessionStorage.getItem(CURRENT_USER));
+            this.store.update({ currentUser: user });
+        }
+        return user;
     }
 }
